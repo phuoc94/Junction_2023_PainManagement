3750b6a4356d30a58ff77283d68c3d0046cdf1d9
Xuefeng Wu <sirowood@gmail.com>
2023-11-11 18:02:16 +0200

    services - user service

---
+++ client/src/services/userService.ts
@@ -0,0 +1,63 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const baseUrl = "http://localhost:3000/api/v1/users";
+
+const tokenId = localStorage.getItem("tokenId");
+
+type GetMyAchievements = {
+  _id: string,
+  name: string,
+  description: string,
+  img_url: string,
+}[];
+
+type GetMyApproaches = {
+  _id: string,
+  name: string,
+  description: string,
+  img_url: string,
+  status: string,
+}[];
+
+type AddApproachToMeResponse = {
+  _id: string,
+  userId: string,
+  approachId: string,
+  status: string,
+};
+
+const getMyAchievements = async () => {
+  try {
+    const { data } = await axios.get<GetMyAchievements>(`${baseUrl}/achievements`, { headers: { "Authorization": `Bearer ${tokenId}` } });
+    console.log(data);
+  } catch (e: unknown) {
+    const error: AxiosError = e as AxiosError;
+    console.log(error.response?.data);
+  }
+}
+
+const getMyApproaches = async () => {
+  try {
+    const { data } = await axios.get<GetMyApproaches>(`${baseUrl}/approaches`, { headers: { "Authorization": `Bearer ${tokenId}` } });
+    console.log(data);
+  } catch (e: unknown) {
+    const error: AxiosError = e as AxiosError;
+    console.log(error.response?.data);
+  }
+}
+
+const addApproachToMe = async (approachId: string) => {
+  try {
+    const { data } = await axios.post<string, AxiosResponse<AddApproachToMeResponse>>(`${baseUrl}/approaches`, { approachId }, { headers: { "Authorization": `Bearer ${tokenId}` } });
+    console.log(data);
+  } catch (e: unknown) {
+    const error: AxiosError = e as AxiosError;
+    console.log(error.response?.data);
+  }
+}
+
+export {
+  getMyAchievements,
+  getMyApproaches,
+  addApproachToMe,
+};
