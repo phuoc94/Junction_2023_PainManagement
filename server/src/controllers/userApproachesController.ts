e609e92d52d81d97d478f3be6cc0b71a1a7d7ae0
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-11 21:03:18 +0200

    Fix approachId validation and add findById method to approachesServices

--- server/src/controllers/userApproachesController.ts
+++ server/src/controllers/userApproachesController.ts
@@ -2,10 +2,10 @@ import type { NextFunction, Request, Response } from 'express'
 import mongoose from 'mongoose'
 
 import userApproachModel from '../models/userApproachModel.js'
+import approachesServices from '../services/approachesServices.js'
 import painsServices from '../services/painsServices.js'
 import UsersServices from '../services/usersServices.js'
 import { ApiError } from '../utils/ApiError.js'
-import approachesServices from '../services/approachesServices.js'
 
 export async function createUserApproach(
   req: Request,
@@ -15,28 +15,30 @@ export async function createUserApproach(
   const { userId } = req.params
   const { approachId } = req.body
 
+  if (approachId === undefined) {
+    next(ApiError.badRequest('approachId is missing'))
+    return
+  }
 
-    if (approachId === undefined) {
-      next(ApiError.badRequest('approachId is missing'))
-      return;
-    }
+  const approach = await approachesServices.findById(approachId)
 
-    if (approachesServices.findById(approachId) === null) {
-      next(ApiError.notFound("This approach with this approach id does not exist!"))
-      return;
-    }
-
-    const newUserApproach = await UsersServices.createUserApproach(
-      userId,
-      approachId
+  if (approach === null) {
+    next(
+      ApiError.notFound('This approach with this approach id does not exist!')
     )
-      
-    if (newUserApproach === null) {
-      next(ApiError.badRequest("This approach cannot be saved!"));
-      return;
-    }
-    res.status(201).json(newUserApproach)
+    return
+  }
 
+  const newUserApproach = await UsersServices.createUserApproach(
+    userId,
+    approachId
+  )
+
+  if (newUserApproach === null) {
+    next(ApiError.badRequest('This approach cannot be saved!'))
+    return
+  }
+  res.status(201).json(newUserApproach)
 }
 
 export async function findAllUserApproaches(
--- server/src/services/approachesServices.ts
+++ server/src/services/approachesServices.ts
@@ -4,23 +4,21 @@ import ApproachRepo from '../models/approachModel.js'
 
 async function findAll() {
   const approaches = await ApproachRepo.find().exec()
-  return approaches 
+  return approaches
 }
 
-async function findById (id : string) {
-
+async function findById(id: string) {
+  const newid = new mongoose.Types.ObjectId(id)
   try {
+    const approach = await ApproachRepo.findById(newid)
 
-    const approach = await ApproachRepo.findById(id);
-
-    return approach;
-
+    return approach
   } catch (e) {
-    return null;
+    return null
   }
 }
 
 export default {
   findAll,
-  findById
+  findById,
 }
