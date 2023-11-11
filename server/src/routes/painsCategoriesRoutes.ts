863f02323570595a5559a1d93b37741592e8a0fb
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-11-11 12:25:01 +0200

    Fix pains routes and categories routes import

--- server/src/routes/painsCategoriesRoutes.ts
+++ server/src/routes/painsCategoriesRoutes.ts
@@ -1,13 +1,10 @@
 import express from 'express'
 
 import painsCategoriesController from '../controllers/painsCategoriesController.js'
-import painsController from '../controllers/painsController.js'
 
 const router = express.Router()
 
 router.get('/', painsCategoriesController.findAllPainsCategories)
 router.get('/:categoryId', painsCategoriesController.findPainById)
-
-router.get('/:categoryId/pain', painsController.findPainsByCategory)
 
 export default router
