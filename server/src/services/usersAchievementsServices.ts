bc0158ef4c03be367205839c7dd5ea62aadba886
Thong Dang <duythong2708@gmail.com>
2023-11-11 09:47:42 +0200

    Remove password for userId when fetching from userAchievementModel!

--- server/src/services/usersAchievementsServices.ts
+++ server/src/services/usersAchievementsServices.ts
@@ -1,4 +1,5 @@
 import userAchievementModel from '../models/userAchievementModel.js'
+import userModel from '../models/userModel.js';
 import { type Achievement } from '../types/Achievement.js'
 
 async function findAllAchievementsByUser(
@@ -8,7 +9,11 @@ async function findAllAchievementsByUser(
     .find({
       userId,
     })
-    .populate('userId')
+    .populate({
+      path: 'userId',
+      model: userModel,
+      select: 'name email ',
+    })
     .populate('achievementId')
   return achievements as unknown as Achievement[]
 }
